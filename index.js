ac930ee148ef255a8e2dda2206acea19066e6fbd
HEPHZIBAH AKPAMIDA <78078959+Hephzibah-script@users.noreply.github.com>
2024-10-03 14:25:54 +0100

    commit all file!

---
+++ index.js
@@ -0,0 +1,88 @@
+// function name(firstName,lastName) {
+//     alert("Hurray, you passed!")
+//     let fullName = firstName + lastName; 
+//     return fullName;
+// };
+// console.log((prompt("what is your name?")));
+
+
+// function myProfile(details, age) {
+//     let currentProfile = details + age; 
+//     return currentProfile;
+// };
+// console.log(prompt("Give us your details, and secure a scholarship abroad"),
+// prompt("Give us your age"));
+
+
+
+// function myWork(name, age, address) {
+//     let currentWork = name + age + address; 
+//     return currentWork;
+// };
+// console.log(prompt("What is your name"),
+// prompt("Give us your address"),
+// prompt("How old are you"));
+
+// let age = 30;
+// let fullname = 'Vic Ajayi';
+
+//IF STATEMENT
+// if (age == "3") {
+//  console.log("not Qualified!");
+// } else if (fullname == "Tobi Toe" ) {console.log("Qualified!");}
+// else {console.log("Sorry, not qualified!");}
+
+
+//SWITCH STATEMENT
+// switch (fullname) {
+//     case "Victor Ajayi":
+//         console.log("First born");
+//         console.log("Qualified!");
+//     break;
+//     case "Seyi Ajayi":
+//         console.log("Last born");
+//         console.log("Qualified!");
+
+//     default: console.log("Member of Ajayi Family");
+// }
+
+// for(i = 0; i <= 5; i++){
+//     console.log(i);  
+// }
+// let result = [1,2,3,4,5,6,7,8,9];
+// for(i = 0; i < result.length; i++){
+    
+//     if (i < 3){
+        
+//         console.log("You made it");
+//     }
+//     else if(i >= 4){console.log("Try again");
+//     }
+
+// }
+
+// create a var called x, and give it a list of fruits: 4mangoes and 4pear. Using for loop bring out
+// You won a mango and you won a pear
+// let result = ['mango', 'mango', 'mango', 'mango', 'pear', 'pear', 'pear', 'pear'];
+// for(i = 0; i < result.length; i++){
+//     if (i < 4){
+//         console.log("You won a mango");
+//     }
+//     else if(i >= 4){console.log("You won a peer")
+//     }
+// }
+// let x = ["vicky", "Daniel", "David"];
+// function sayMyName(){
+//     console.log(x);
+// }
+// sayMyName(x)
+
+   
+function x(firstName, lastName){
+    console.log("----------")
+    console.log("Hello" + " " + firstName + " " + lastName + "!");
+    console.log("----------")
+}
+x("Bob", "Marley");
+
+
